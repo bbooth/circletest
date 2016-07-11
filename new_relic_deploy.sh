e942805ad618054e74e2cd19a9c03218c380b94c
Bradley Booth <bradley.booth@gmail.com>
2016-07-11 16:34:16 -0700

    fix circle

--- new_relic_deploy.sh
+++ new_relic_deploy.sh
@@ -2,5 +2,4 @@
 curl -X POST https://api.newrelic.com/v2/applications/${NEW_RELIC_APPLICATION_ID}/deployments.json \
      -H "X-Api-Key:${NEW_RELIC_API_KEY}" -i \
      -H 'Content-Type: application/json' \
-     -d "{\"deployment\":{\"revision\":\"Circle CI Build ${CIRCLE_BUILD_NUM}\"}}"  \
--v --trace-ascii /dev/stdout
+     -d "{\"deployment\":{\"revision\":\"Circle CI Build: ${CIRCLE_BUILD_NUM}\nGit Commit: ${CIRCLE_SHA1}\"}}"
