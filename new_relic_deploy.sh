8c432a4ded4257d568a89ed7edba3f04b505e27e
Bradley Booth <bradley.booth@gmail.com>
2016-07-11 16:08:11 -0700

    fix circle

---
+++ new_relic_deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+curl -X POST 'https://api.newrelic.com/v2/applications/19789137/deployments.json' \
+     -H 'X-Api-Key:69ee32549fcde1c4e1744ca320ff2240160c8209cc21eac' -i \
+     -H 'Content-Type: application/json' \
+     -d '{"deployment":{"revision":"test"}}'  \
+-v --trace-ascii /dev/stdout
