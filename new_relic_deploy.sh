89d33ee614146bde9c335af33650e5d7c5c04adb
Bradley Booth <bradley.booth@gmail.com>
2016-09-14 14:02:36 -0700

    test mem calc

--- new_relic_deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-curl -X POST https://api.newrelic.com/v2/applications/${NEW_RELIC_APPLICATION_ID}/deployments.json \
-     -H "X-Api-Key:${NEW_RELIC_API_KEY}" -i \
-     -H 'Content-Type: application/json' \
-     -d "{\"deployment\":{\"revision\":\"Circle CI Build: ${CIRCLE_BUILD_NUM}\nGit Commit: ${CIRCLE_SHA1}\"}}"
